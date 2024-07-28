3904d4ff25e0aede4bf148096d3bb00c75cd4a98
lvm5 <lvm5>
2024-07-28 18:27:32 -0300

    Custom tab view with clip shape or capsule

---
+++ CustomTabView/CustomTabs.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabs.swift
+//  CustomTabView
+//
+//  Created by Leandro Morais on 28/07/24.
+//
+
+import SwiftUI
+
+struct CustomTabs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabs()
+}
